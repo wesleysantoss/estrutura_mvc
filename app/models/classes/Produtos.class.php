531e5be9c6d8ca2707f72126e30210866350c4ee
Wesley Santos <wessleysanttos@live.com>
2018-05-21 14:10:01 -0300

    Exemplo de classe

--- app/models/classes/Produtos.class.php
+++ app/models/classes/Produtos.class.php
@@ -1,7 +1,40 @@
 <?php
 
+require_once PATH_SERVER . "models/dao/UsuarioDao.php";
 
+class Usuario 
+{
+	private $UsuarioDao;
 
+	/**
+	* Abre conexão com o banco de dados.
+	*/
+	private function abreConexaoBD()
+	{
+		$this->UsuarioDao = new UsuarioDao;
+	}
 
+	/**
+	* Fecha a conexão com o banco de dados.
+	*/
+	private function fechaConexaoBD()
+	{
+		unset($this->UsuarioDao);
+	}
+  
+  public function buscarProdutos()
+  {
+		// Abre conexão com o BD.
+		$this->abreConexaoBD();
+
+		$produtos = $this->ConexaoBD->getAllProdutos();
+
+		// Fecha conexão com o BD.
+		$this->fechaConexaoBD();
+
+		return $produtos;
+  }
+  
+}
 
 ?>
