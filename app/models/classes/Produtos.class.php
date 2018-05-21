acd727ea10ab51e986baddf4c1fa818250dc9866
Wesley Santos <wessleysanttos@live.com>
2018-05-21 14:10:55 -0300

    Update Produtos.class.php

--- app/models/classes/Produtos.class.php
+++ app/models/classes/Produtos.class.php
@@ -21,9 +21,9 @@ class Usuario
 	{
 		unset($this->UsuarioDao);
 	}
-  
-  public function buscarProdutos()
-  {
+
+	public function buscarProdutos()
+	{
 		// Abre conexão com o BD.
 		$this->abreConexaoBD();
 
@@ -33,8 +33,8 @@ class Usuario
 		$this->fechaConexaoBD();
 
 		return $produtos;
-  }
-  
+	}
+
 }
 
 ?>
