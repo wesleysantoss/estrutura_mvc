4a2fefb3ff4753cc8817ae5232a68b45ef09477b
Wesley Santos <wessleysanttos@live.com>
2018-05-21 14:17:56 -0300

    Update Constante.php

--- app/config/Constante.php
+++ app/config/Constante.php
@@ -9,4 +9,12 @@ define('PATH', 		'http://127.0.0.1/mvc/App');
 define('PROTOCOL',  'https://');
 define('DS'      ,   DIRECTORY_SEPARATOR);
 
+// ----------------------------------------------------------
+// Base de dados.
+// ----------------------------------------------------------
+define("SITE_HOST"    , "localhost");
+define("SITE_DATABASE", "teste");
+define("SITE_LOGIN"   , "teste");
+define("SITE_SENHA"   , "admin");
+
 ?>
