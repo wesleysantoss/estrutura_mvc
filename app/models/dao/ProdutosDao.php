9c993be927a644a3bee186e95cb1e19992ab2ca9
Wesley Santos <wessleysanttos@live.com>
2018-05-21 14:13:27 -0300

    Update ProdutosDao.php

--- app/models/dao/ProdutosDao.php
+++ app/models/dao/ProdutosDao.php
@@ -1,7 +1,25 @@
 <?php
 
+require_once PATH_SERVER . "config/ConexaoBD_Site.php";
 
+class ProdutoDao	 
+{
+  public function __construct()
+  {
+    $this->pdo = ConexaoBDSite::getConnection();
+  }
 
+  public function __destruct()
+  {
+    unset($this->pdo);
+  }
 
-
+  /**
+  * Função que pega todos os produtos.
+  */
+  public function getAllProdutos()
+  {
+     // Código SQL.
+  }
+}
 ?>
